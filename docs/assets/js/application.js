372382dd22f44232b7bf813f9c64765dbfca5914
Arnold Daniels <arnold@jasny.net>
2012-10-11 19:46:49 +0200

    Modified customization to work with Jasny extensions

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -83,9 +83,29 @@
       inputsPlugin.attr('checked', !inputsPlugin.is(':checked'))
     })
 
+    $('#components.download .toggle-jasny').on('click', function (e) {
+      e.preventDefault()
+      inputsComponent.attr('checked', false)
+      $('#components.download input.jasny').attr('checked', true)
+    })
+
+    $('#plugins.download .toggle-jasny').on('click', function (e) {
+      e.preventDefault()
+      inputsPlugin.attr('checked', false)
+      $('#plugins.download input.jasny').attr('checked', true)
+    })
+
     $('#variables.download .toggle-all').on('click', function (e) {
       e.preventDefault()
       inputsVariables.val('')
+    })
+
+    $('#plugins.download input[value="bootstrap-popover.js"]').on('click', function (e) {
+      if ($(this).is(':checked')) $('#plugins.download input[value="bootstrap-tooltip.js"]').attr('checked', true)
+    })
+
+    $('#plugins.download input[value="bootstrap-tooltip.js"]').on('click', function (e) {
+      if ($(this).is(':not(:checked)')) $('#plugins.download input[value="bootstrap-popover.js"]').attr('checked', false)
     })
 
     // request built javascript
@@ -99,7 +119,25 @@
             .toArray()
         , vars = {}
         , img = ['glyphicons-halflings.png', 'glyphicons-halflings-white.png']
+        , fonts = []
+        , autoselect = {
+            'jasny/modals.less': ['modals.less']
+          , 'jasny/layouts-semifluid.responsive-1200px-min.less': ['jasny/layouts-semifluid.less', 'responsive-1200px-min.less']
+          , 'jasny/forms.responsive-767px-max.less': ['jasny/forms.less', 'responsive-767px-max.less']
+          , 'jasny/forms.responsive-768px-979px.less': ['jasny/forms.less', 'responsive-768px-979px.less']
+          , 'jasny/forms.responsive-1200px-min.less': ['jasny/forms.less', 'responsive-1200px-min.less']
+          , 'jasny/page-alert.responsive-767px-max.less': ['jasny/page-alert.less', 'responsive-767px-max.less']
+          , 'jasny/page-alert.responsive-1200px-min.less': ['jasny/page-alert.less', 'responsive-1200px-min.less']
+        }
+        
+    if ($('#components.download input[value="jasny/iconic.less"]').is(':checked'))
+      fonts = ['iconic_fill.eot', 'iconic_fill.otf', 'iconic_fill.svg', 'iconic_fill.ttf', 'iconic_fill.woff', 'iconic_stroke.eot', 'iconic_stroke.otf', 'iconic_stroke.svg', 'iconic_stroke.ttf', 'iconic_stroke.woff']
 
+    $.map(autoselect, function(deps, file) {
+      if ($.map(deps, function (value) { return $('#components.download input[value="'+value+'"]').is(':checked') ? 1 : null }).length == deps.length)
+        css.push(file)
+    })
+    
     $("#variables.download input")
       .each(function () {
         $(this).val() && (vars[ $(this).prev().text() ] = $(this).val())
@@ -107,13 +145,14 @@
 
       $.ajax({
         type: 'POST'
-      , url: /\?dev/.test(window.location) ? 'http://localhost:3000' : 'http://bootstrap-server.jasny.net'
+      , url: /\?dev|^http:\/\/localhost\//.test(window.location) ? 'http://localhost:3000' : 'http://bootstrap-server.jasny.net'
       , dataType: 'jsonpi'
       , params: {
           js: js
         , css: css
         , vars: vars
         , img: img
+        , fonts: fonts
       }
       })
     })
