197a8135c4568a6418dcd16cb1f86872af634592
joshjb84 <joshjb84@hotmail.com>
2014-04-05 19:10:24 +0800

    Fix to naming in inputmask.js and tests
    Changed the name of Input.DEFAULS to Input.DEFAULTS and added unit tests
    around how the options are set up.

--- js/tests/unit/inputmask.js
+++ js/tests/unit/inputmask.js
@@ -1,6 +1,13 @@
 $(function () {
 
-    module('inputmask')
+      var $body;
+
+      module('inputmask', {
+        setup : function() {
+          $body = $(document.body);
+          $body.removeData('inputmask');
+        }
+      })
 
       test('should provide no conflict', function () {
         var inputmask = $.fn.inputmask.noConflict()
@@ -15,6 +22,60 @@ $(function () {
       test('should return element', function () {
         ok($(document.body).inputmask()[0] == document.body, 'document.body returned')
       })
-      
+
+      test('should use default mask', function() {
+        var expected = ""
+        $.fn.inputmask.Constructor.DEFAULTS.mask = expected
+
+        $body.inputmask()
+
+        equal(expected, $body.data('inputmask').options.mask)
+      })
+
+      test('should use default placeholder', function() {
+        var expected = "_"
+        $.fn.inputmask.Constructor.DEFAULTS.placeholder = expected
+
+        $body.inputmask()
+
+        equal(expected, $body.data('inputmask').options.placeholder)
+      })
+
+      test('should use default definitions', function() {
+        var expected = {
+          '9': "[0-9]",
+          'a': "[A-Za-z]"
+        }
+        $.fn.inputmask.Constructor.DEFAULTS.definitions = expected
+
+        $body.inputmask()
+
+        deepEqual(expected, $body.data('inputmask').options.definitions)
+      })
+
+      test('should override mask when options.mask provided', function() {
+        var expected = '99-99';
+        $body.inputmask({ mask: expected})
+
+        equal(expected, $body.data('inputmask').options.mask)
+      })
+
+      test('should override placeholder when options.placeholder provided', function() {
+          var expected = '-';
+          $body.inputmask({ placeholder: expected})
+
+          equal(expected, $body.data('inputmask').options.placeholder)
+      })
+
+      test('should override definitions when options.definitions provided', function() {
+        var expected = {
+          '9': "[0-9]",
+          'a': "[A-Za-z]"
+        }
+
+        $body.inputmask({definitions: expected})
+
+        deepEqual(expected, $body.data('inputmask').options.definitions)
+      })
       // TODO: add inputmask tests
 })
