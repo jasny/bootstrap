351944a19ee70aa8bf8e3e61aef78fedf764df95
Andrii Cherytsya <poratuk@gmail.com>
2016-09-27 13:23:01 +0300

    Fixed grunt warnings.

--- js/tests/unit/rowlink.js
+++ js/tests/unit/rowlink.js
@@ -1,20 +1,21 @@
-$(function () {
+$(function ()
+{
 
-    module('rowlink')
+  module('rowlink')
 
-      test('should provide no conflict', function () {
-        var rowlink = $.fn.rowlink.noConflict()
-        ok(!$.fn.rowlink, 'rowlink was set back to undefined (org value)')
-        $.fn.rowlink = rowlink
-      })
-
-      test('should be defined on jquery object', function () {
-        ok($(document.body).rowlink, 'rowlink method is defined')
-      })
-
-      test('should return element', function () {
-        ok($(document.body).rowlink()[0] == document.body, 'document.body returned')
-      })
-
-      // TODO: add rowlink tests
+  test('should provide no conflict', function ()
+  {
+    var rowlink = $.fn.rowlink.noConflict()
+    ok(!$.fn.rowlink, 'rowlink was set back to undefined (org value)')
+    $.fn.rowlink = rowlink
+  })
+  test('should be defined on jquery object', function ()
+  {
+    ok($(document.body).rowlink, 'rowlink method is defined')
+  })
+  test('should return element', function ()
+  {
+    ok($(document.body).rowlink()[0] == document.body, 'document.body returned')
+  })
+  // TODO: add rowlink tests
 })
