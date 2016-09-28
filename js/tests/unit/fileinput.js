d11b5f4c84412610b63c336d0db17b82ad888d5a
Andrii Cherytsya <poratuk@gmail.com>
2016-09-28 11:36:50 +0300

    Fixed input test

--- js/tests/unit/fileinput.js
+++ js/tests/unit/fileinput.js
@@ -14,7 +14,11 @@ $(function ()
   })
   test('should return element', function ()
   {
-    ok($(document.body).fileinput()[0] == document.body, 'document.body returned')
+    ok($(document.body).append('<div class="fileinput fileinput-new input-group" data-provides="fileinput">'
+   + '<div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>'
+  + '<span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select file</span><span class="fileinput-exists">Change</span><input type="file" name="..."></span>'
+  + '<a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>'
+ + '</div>').fileinput()[0] == document.body, 'document.body returned');
   })
   // TODO: add fileinput tests
 })
