2d52ad1a01e15718fb0181e8a665462ff0b5b9d8
Sergey Boltonosov <minstel@yandex.ru>
2019-02-11 23:00:56 +0700

    Update docs to v4.0.0

--- assets/js/application.js
+++ assets/js/application.js
@@ -48,29 +48,10 @@
       e.preventDefault()
     })
 
-    // back to top
-    setTimeout(function () {
-      var $sideBar = $('.bs-sidebar')
-
-      $sideBar.affix({
-        offset: {
-          top: function () {
-            var offsetTop      = $sideBar.offset().top
-            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-            var navOuterHeight = $('.bs-docs-nav').height()
-
-            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-          },
-          bottom: function () {
-            return (this.bottom = $('.bs-footer').outerHeight(true))
-          }
-        }
-      })
-    }, 100)
-
-    setTimeout(function () {
-      $('.bs-top').affix()
-    }, 100)
+    $('.bs-sidebar').stick_in_parent({
+      offset_top: 56,
+      spacer: false
+    });
   })
 
 }(jQuery)
